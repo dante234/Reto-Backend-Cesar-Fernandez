2be76794bdaaad7bfd08b72580b34688e8b31a41
===)dante== <cesarfdperu@gmail.com>
2024-10-17 03:24:13 -0500

    agregando variables de entorno4

--- src/main/resources/db/migration/V2__insert_data.sql
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -1,7 +1,7 @@
 INSERT INTO candidates (name, email, gender, salary_expected, position, status) VALUES
 ('Cesar Fernandez', 'cesarfernadez@example.com', 'Male', 50000, 'Java Developer', 'Applied'),
-('Kimberlin abreu', 'KimberlinAbreu@example.com', 'Female', 60000, 'Designer UX', 'Interviewing');
+('Kimberlin abreu', 'KimberlinAbreu@example.com', 'Female', 60000, 'Designer UX', 'Interviewing'),
 ('Gerardo Bracho', 'gerardobracho@example.com', 'Male', 50000, 'Fronted Developer', 'Applied'),
-('Andreina Fernandez', 'andreinafernadez@example.com', 'Female', 60000, 'Java Developer', 'Interviewing');
+('Andreina Fernandez', 'andreinafernadez@example.com', 'Female', 60000, 'Java Developer', 'Interviewing'),
 ('Mateo Fernandez', 'mateofernadez@example.com', 'Male', 50000, 'Fronted Developer', 'Applied'),
 ('Ana Fernandez', 'anafernadez@example.com', 'Female', 60000, 'Java Developer', 'Interviewing');
